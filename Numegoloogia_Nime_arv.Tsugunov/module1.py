1a8bf9dbb56d3ed03878a0ab4e1ccae44b958eda
TsugunovS <stanislav.tsugunov@gmail.com>
2023-03-13 12:32:42 +0200

    13/03/23

---
+++ Numegoloogia_Nime_arv.Tsugunov/module1.py
@@ -0,0 +1,25 @@
+﻿def name():
+    nime = {"а":1, "б":2, "в":3, "г":4, "д":5, "е":6, "ё":7, "ж":8, "з":9,
+            "и":1, "й":2, "к":3, "л":4, "м":5, "н":6, "о":7, "п":8, "р":9,
+            "с":1, "т":2, "у":3, "ф":4, "х":5, "ц":6, "ч":7, "ш":8, "щ":9,
+            "ъ":1, "ы":2, "ь":3, "э":4, "ю":5, "я":6}
+    
+    filename = "nimi.txt"
+    name_value = []
+    
+    with open(filename, "r") as fa:
+        for name in fa:
+            name = name.strip().lower()
+            print(f"Nimi: {name}")
+            for letter in name:
+                if letter in nime:
+                    value = nime[letter]
+                    name_value.append(value)
+                    print(f"{letter},{value}")
+    
+            name_sum = sum(name_value)
+            while name_sum > 9:
+                name_sum = sum(int(digit) for digit in str(name_sum))
+                
+            print(f"Числовое значение вашего имени равно {name_sum}\n")
+            name_value = []
